286946cb45a313fb50acff6b792990b6b4ebdcac
Giovanni Gobbi <giovannigobbibusiness@gmail.com>
2020-05-19 07:50:55 +0200

    Add multiple devices

--- app/src/androidTest/java/com/giovannigobbi/androidtestingexample/InstrumentedUnitTest.java
+++ app/src/androidTest/java/com/giovannigobbi/androidtestingexample/InstrumentedUnitTest.java
@@ -6,12 +6,9 @@ import android.content.SharedPreferences;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertEquals;
 
 public class InstrumentedUnitTest {
