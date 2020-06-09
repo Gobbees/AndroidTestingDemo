55e7fa6f13521697f7d8dcd34e094f16dfb2589e
Giovanni Gobbi <giovannigobbibusiness@gmail.com>
2020-06-09 17:20:30 +0200

    Change wrong filename

--- app/src/androidTest/java/com/giovannigobbi/androidtestingexample/InstrumentedIntegrationTest.java
+++ app/src/androidTest/java/com/giovannigobbi/androidtestingexample/InstrumentedIntegrationTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class InstrumentedUnitTest {
+public class InstrumentedIntegrationTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class) {
